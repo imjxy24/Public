48b7689579dee3f338735b9d40cad411cceccc4d
J A Y <141551664+imjxy24@users.noreply.github.com>
2024-04-24 22:07:55 +0530

    Update vars.js

--- shortener/organic/vars.js
+++ shortener/organic/vars.js
@@ -1,7 +1,7 @@
 // add vars
 const IMAGE_URL = "";
 const SEARCH_PAGE_URL = "";
-var count = 3;
-const POST1 = "";
-const POST2 = "";
-const POST3 = "";
+var count = 10;
+const MPOST = "";
+const LPOST = "";
+var POST;
